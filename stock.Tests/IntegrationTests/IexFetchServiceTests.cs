16b7aee1b30191c77a115968ab6ebf88823c8e7b
bbastanza <b.bastanza@gmail.com>
2021-01-16 16:35:12 -0800

    commit fix iex integration tests

--- stock.Tests/IntegrationTests/IexFetchServiceTests.cs
+++ stock.Tests/IntegrationTests/IexFetchServiceTests.cs
@@ -1,7 +1,7 @@
 using System.IO;
 using Core.Services.IexServices;
+using Infrastructure.Exceptions;
 using Microsoft.Extensions.Configuration;
-using Moq;
 using NUnit.Framework;
 
 namespace API.Tests.IntegrationTests
@@ -17,27 +17,25 @@ namespace API.Tests.IntegrationTests
             var apiHelper = new ApiHelper();
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("stock.Tests/IntegrationTests/appsettings.Test.json", optional: false, reloadOnChange: false);
+                .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: false);
             
             _iexFetchService = new IexFetchService(apiHelper, config.Build());
         }
 
-        [Ignore("cannot find appsettings.Test.json")]
         [Test]
-        public void GetStockBySymbol_WhenCalled_GetsValidIexData()
+        public void GetStockBySymbol_ValidSymbol_GetsValidIexData()
         {
-            // "Tpk_78fdb3ab586845aba561ebf6c43c96e0"
-            // _configurationSection.Setup(x => x.Path).Returns("IexKeys");
-            // _configurationSection.Setup(x => x.Key).Returns("TestKey");
-            // _configurationSection.Setup(x => x.Value).Returns("Tpk_78fdb3ab586845aba561ebf6c43c96e0");
-            // _configuration.Setup(a => a.GetSection(It.Is<string>(s => s == "IexKeys:TestKey")))
-            //     .Returns(_configurationSection.Object);
-
             var result = _iexFetchService.GetStockBySymbol("IBM");
 
             Assert.That(result.Symbol, Is.EqualTo("IBM"));
-            Assert.That(result.Symbol, Is.EqualTo("International Business Machines Corp."));
+            Assert.That(result.CompanyName, Is.EqualTo("International Business Machines Corp."));
             Assert.That(result.LatestPrice, Is.InstanceOf<double>());
+        }
+        
+        [Test]
+        public void GetStockBySymbol_InvalidSymbol_ThrowsIexException()
+        {
+            Assert.That(() => _iexFetchService.GetStockBySymbol("Invalid Stock"), Throws.Exception.TypeOf<IexException>());
         }
     }
 }
